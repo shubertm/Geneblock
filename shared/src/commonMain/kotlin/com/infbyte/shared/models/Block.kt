6d8dda2c2a594ed4a55acac505aedf09c1ccc9d2
Shubert Munthali <shubertm.m@gmail.com>
2025-08-29 17:24:48 +0200

    Make sure DB can hold data (#7)

--- shared/src/commonMain/kotlin/com/infbyte/shared/models/Block.kt
+++ shared/src/commonMain/kotlin/com/infbyte/shared/models/Block.kt
@@ -1,24 +1,21 @@
 package com.infbyte.shared.models
 
+import io.ktor.util.date.getTimeMillis
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Block(
     val hash: String,
     val date: Long,
-    val height: Int,
-    val depth: Int,
-    val distance: Long,
-    val capacity: Float,
     val size: Int,
     val currency: Currency,
-    val value: Float,
-    val valueToday: Float,
-    val input: Currency,
-    val output: Currency,
     val transactions: Int,
-    val inputs: Int,
-    val outputs: Int,
     val miner: String,
     val reward: Float
-)
+) {
+    private var distance: Long = 0L
+
+    fun calculateDistance() {
+        distance = getTimeMillis() - date
+    }
+}
