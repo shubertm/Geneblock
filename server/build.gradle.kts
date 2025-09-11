48bd92dcccd36c1793cd633d4b1e0701482745b8
Shubert Munthali <shubertm.m@gmail.com>
2025-09-11 04:51:07 +0200

    Remove Java versions from server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,16 +21,6 @@ ktor {
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
-}
-
 dependencies {
     implementation(project(":shared"))
     implementation(libs.ktor.server.core)
