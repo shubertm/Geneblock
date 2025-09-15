66e2344a5bf9a49095b75ee297d4d899aced27e2
Shubert Munthali <shubertm.m@gmail.com>
2025-09-15 18:13:18 +0200

    Fix JVM target workflow compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,11 +81,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
     }
 }
 
